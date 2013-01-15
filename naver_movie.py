dc9e857d78db94c71122cd83ee6d8076984a6d93
sungchi <sungchi9@gmail.com>
2013-01-15 18:23:52 +0900

    init

--- naver_movie.py
+++ naver_movie.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-url = 'http://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20130114&page='
-for i in range(21):
+base_url = 'http://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20130114&page='
+url = base_url 
+
+for i in range(20):
     url = url+str(i)
     handle = urllib2.urlopen(url)
     data = handle.read()
     soup = BeautifulSoup(data)
     for text in soup.find_all("div",class_="tit5"):
         print(text.contents[1].string.encode("utf-8"))
-    url = 'http://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20130114&page='
+    url = base_url 
